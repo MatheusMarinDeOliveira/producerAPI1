41aa191b665b8cad9c9f4bedcd0377ff3398c46e
Matheus <matheus.marin@usp.br>
2020-07-05 16:02:41 -0300

    make it better

--- src/main/java/infrastructure/rabbitmq/RabbitMQConfigurations.java
+++ src/main/java/infrastructure/rabbitmq/RabbitMQConfigurations.java
@@ -4,12 +4,16 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RabbitMQConfigurations {
@@ -39,7 +43,7 @@ public class RabbitMQConfigurations {
     }
 
     @Bean
-    public RabbitTemplate amqpTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory  connectionFactory) {
+    public RabbitTemplate amqpTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
         return rabbitTemplate;
@@ -50,4 +54,23 @@ public class RabbitMQConfigurations {
         return new Jackson2JsonMessageConverter();
     }
 
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        final URI rabbitMqUrl;
+        try {
+            rabbitMqUrl = new URI("amqp://pbinmofj:5gvlRptD4d8gi-WCMZjfpH8GJ20aOnV0@buck.rmq.cloudamqp.com/pbinmofj");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUsername(rabbitMqUrl.getUserInfo().split(":")[0]);
+        factory.setPassword(rabbitMqUrl.getUserInfo().split(":")[1]);
+        factory.setHost(rabbitMqUrl.getHost());
+        factory.setPort(rabbitMqUrl.getPort());
+        factory.setVirtualHost(rabbitMqUrl.getPath().substring(1));
+
+        return factory;
+    }
 }
